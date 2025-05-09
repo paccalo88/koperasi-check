39f66058efbfd8676032d689475182180753cd15
paccalo88 <pentolanpansit@yahoo.com>
2025-05-09 22:16:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let senaraiIC = [];
+
+fetch('anggota.xlsx')
+  .then(res => res.arrayBuffer())
+  .then(data => {
+    const workbook = XLSX.read(data, { type: "array" });
+    const sheet = workbook.Sheets[workbook.SheetNames[0]];
+    const json = XLSX.utils.sheet_to_json(sheet);
+
+    senaraiIC = json.map(row => row['NO.K/P']?.replace(/-/g, '').trim());
+  });
+
+function semakIC() {
+  const ic = document.getElementById("icInput").value.replace(/-/g, '').trim();
+  const result = document.getElementById("result");
+
+  if (!ic) {
+    result.textContent = "Sila masukkan nombor kad pengenalan.";
+    return;
+  }
+
+  if (senaraiIC.includes(ic)) {
+    result.textContent = "✅ Anda SUDAH berdaftar sebagai ahli koperasi.";
+  } else {
+    result.textContent = "❌ Anda BELUM berdaftar sebagai ahli koperasi.";
+  }
+}
